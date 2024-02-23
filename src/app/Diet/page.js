9273316da8667542f2ac0f037352f7512c3f5c7f
Mslonoff <matthew.slonoff@gmail.com>
2024-02-23 18:05:16 -0500

    functionality added to days
    Co-authored-by: Dave Robinson <dave-rob@users.noreply.github.com>

--- src/app/Diet/page.js
+++ src/app/Diet/page.js
@@ -1,4 +1,52 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+
+const Day = (props) => {
+  const [weight, setWeight] = useState("");
+  const [calories, setCalories] = useState("");
+
+  function changeWeight(e) {
+    setWeight(e.target.value);
+  }
+
+  function changeCalories(e) {
+    setCalories(e.target.value);
+  }
+
+  function onSubmit(e) {
+    e.preventDefault();
+    console.log({ weight: weight, calories: calories });
+  }
+
+  return (
+    <div className="border border-gray-400 w-72 m-2 h-64 primary rounded-xl">
+      <h1 className="text-center mb-10">{props.day}</h1>
+      <form className="w-1/2 m-auto text-center">
+        <label className="mt-20">Weight (lbs):</label>
+        <input
+          name="weight"
+          className="w-full mb-3 secondary"
+          type="text"
+          onChange={changeWeight}
+          required
+        ></input>
+        <label className="mt-10">Calories:</label>
+        <input
+          className="w-full secondary"
+          type="text"
+          onChange={changeCalories}
+          required
+        ></input>
+        <button
+          className="mt-4 bg-white text-blue-500 px-4 py-2 rounded-md ml-2"
+          onClick={onSubmit}
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
 
 const page = () => {
   const days = [
@@ -12,23 +60,12 @@ const page = () => {
   ];
   return (
     <>
-      <div className="mt-10 m-auto w-fit primary">
+      <div className="mt-10 m-auto w-fit primary px-64 text-xl">
         I will eat __ calories every day
       </div>
       <div className="flex flex-wrap w-3/4 m-auto mt-4 justify-center ">
         {days.map((day, index) => (
-          <div
-            className="border border-gray-400 w-72 m-2 h-64 primary rounded-xl"
-            key={index}
-          >
-            <h1 className="text-center mb-10">{day}</h1>
-            <form className="w-1/2 m-auto text-center">
-              <label className="mt-20">Weight (lbs):</label>
-              <input className="w-full mb-3 secondary" type="text"></input>
-              <label className="mt-10">Calories:</label>
-              <input className="w-full secondary" type="text"></input>
-            </form>
-          </div>
+          <Day key={index} day={day} />
         ))}
       </div>
     </>
