d6623460eec76d3ad2f56fe74ae1a6aca414956f
derekmanninghm <derekmanninghm@gmail.com>
2024-02-23 15:35:45 -0800

    diverge fix

--- src/app/Home/page.js
+++ src/app/Home/page.js
@@ -27,22 +27,7 @@ const Home = () => {
       <header className="bg-purple-700 bg-opacity-75 px-6 text-white py-2">
         <h1 className="text-lg font-semibold justify-start">FitFusion</h1>
       </header>
-      <section className="w-screen flex justify-center mt-10">
-        <div className="grid grid-cols-4 gap-4 w-1/2 absolute ">
-          <h1 className="text-black ml-5">Dashboard</h1>
-          <h1 className="text-black ml-9">My Goals</h1>
-          <h1 className="text-black ml-32">My Diet</h1>
-          <h1 className="text-black ml-32">My Cardio</h1>
-        </div>
-        <input
-          style={{ "--thumb-color": "#ffffff" }}
-          type="range"
-          min="0"
-          max="100"
-          value="50"
-          className="w-1/2 h-9 bg-gray-200 rounded-full outline-none appearance-none"
-        ></input>
-      </section>
+      <NavBar />
       {/* /* -------------------------Chart JS-----------------------*/}
       <section className="flex justify-center h-screen">
         <div className="w-1/4 relative top-40 right-30 mr-20">
