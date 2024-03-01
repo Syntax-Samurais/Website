4bc763cf74d151ce62a747c50e67eac7ea4f6700
xxjrtxx <xxjrtxx@pop-os.localdomain>
2024-03-01 16:45:26 -0600

    chartUpdates

--- src/app/Home/page.js
+++ src/app/Home/page.js
@@ -26,10 +26,10 @@ const Home = () => {
   const [mileChartData, setMileChartData] = useState(defaultChar);
   const [calorieChartData, setCalorieChartData] = useState(defaultChar);
   const [WeightChartData, setWeightChartData] = useState(defaultChar);
-  const [goalWeight, setGoalWeight] = useState(150);
-  const [weeklyRunGoalState, setRunGoal] = useState(25);
+  const [goalWeight, setGoalWeight] = useState(0);
+  const [weeklyRunGoal, setRunGoal] = useState(0);
   const [totalMilesRan, setMilesRan] = useState(0);
-  const [weeklyCalorieGoal, setCalorieGoal] = useState(25);
+  const [weeklyCalorieGoal, setCalorieGoal] = useState(0);
   const [totalCaloriesConsumed, setCaloriesConsumed] = useState(0);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
@@ -41,20 +41,18 @@ const Home = () => {
         const tempItems = await res.json();
         console.log(tempItems);
 
-        let weeklyRunGoal = 29;
-        let totalMilesRan = 0;
-        tempItems.runData.map(
-          (run) => (totalMilesRan += Number(run.miles_ran)),
-        );
-        setRunGoal(weeklyRunGoal);
-        setMilesRan(totalMilesRan);
+        let weekRunGoal = 0;
+        let totMilesRan = 0;
+        tempItems.runData.map((run) => (totMilesRan += Number(run.miles_ran)));
+        setRunGoal(weekRunGoal);
+        setMilesRan(totMilesRan);
 
         let mileCompletionColor = "#1861A5";
         let chartBGColor = "#718199";
-        if (totalMilesRan > weeklyRunGoal) {
+        if (totMilesRan > weekRunGoal) {
           mileCompletionColor = "#00ff00";
           chartBGColor = "#1861A5";
-        } else if (totalMilesRan === weeklyRunGoal) {
+        } else if (totMilesRan === weekRunGoal) {
           mileCompletionColor = "#1861A5";
           chartBGColor = "#1861A5";
         } else {
@@ -66,10 +64,8 @@ const Home = () => {
           datasets: [
             {
               data: [
-                totalMilesRan,
-                weeklyRunGoal - totalMilesRan <= 0
-                  ? 0
-                  : weeklyRunGoal - totalMilesRan,
+                totMilesRan,
+                weekRunGoal - totMilesRan <= 0 ? 0 : weekRunGoal - totMilesRan,
               ],
               backgroundColor: [mileCompletionColor, chartBGColor],
               borderColor: "#2e2f2e",
@@ -78,20 +74,22 @@ const Home = () => {
           ],
         });
 
-        let weeklyCalorieGoal = tempItems.goals[0].goal_calorie_intake * 7;
-        let totalCaloriesConsumed = 0;
+        // let weekCalGoal = tempItems.goals[0].goal_calorie_intake * 7;
+        let weekCalGoal = 25;
+        let totCalConsumed = 0;
         tempItems.calorieData.map(
-          (meal) => (totalCaloriesConsumed += Number(meal.calories)),
+          (meal) => (totCalConsumed += Number(meal.calories)),
         );
-        setCalorieGoal(weeklyCalorieGoal);
-        setCaloriesConsumed(totalCaloriesConsumed);
+        // setCalorieGoal(weekCalGoal);
+        setCalorieGoal(weekCalGoal);
+        setCaloriesConsumed(totCalConsumed);
 
         let calorieCompletionColor = "#1861A5";
         let calChartBGColor = "#718199";
-        if (totalCaloriesConsumed > weeklyCalorieGoal) {
+        if (totCalConsumed > weekCalGoal) {
           calorieCompletionColor = "#ff0000"; // Change to red if exceeded goal
           calChartBGColor = "#1861A5";
-        } else if (totalCaloriesConsumed === weeklyCalorieGoal) {
+        } else if (totCalConsumed === weekCalGoal) {
           calorieCompletionColor = "#1861A5";
           calChartBGColor = "#1861A5";
         } else {
@@ -102,10 +100,7 @@ const Home = () => {
           labels: ["Calorie Consumed", "Calories Remaining"],
           datasets: [
             {
-              data: [
-                totalCaloriesConsumed,
-                weeklyCalorieGoal - totalCaloriesConsumed,
-              ],
+              data: [totCalConsumed, weekCalGoal - totCalConsumed],
 
               backgroundColor: [calorieCompletionColor, calChartBGColor],
               borderColor: "#2e2f2e",
@@ -142,6 +137,92 @@ const Home = () => {
 
     fetchData();
   }, []);
+
+  const renderCaloriesHeader = () => {
+    if (weeklyCalorieGoal === 0 || weeklyCalorieGoal === null) {
+      return (
+        <div className="text-PrimaryGrey">
+          No Data Found!<br></br>Click{" "}
+          <span className="text-PrimaryBlue font-bold underline">
+            <a href="/goals"> here</a>
+          </span>{" "}
+          to set<br></br> a goal.
+        </div>
+      );
+    } else {
+      return (
+        <div className="w-32 h-32 flex justify-center content-center self-center absolute rounded-full bg-PrimaryGrey shadow-lg">
+          <span className="text-center self-center font-bold text-white">
+            {totalCaloriesConsumed}/<br></br>
+            {weeklyCalorieGoal}
+          </span>
+        </div>
+      );
+    }
+  };
+
+  const renderMilesRan = () => {
+    if (weeklyRunGoal === 0) {
+      return (
+        <h1 className="text-PrimaryGrey space-y-2 ml-64 pl-64">
+          No Run Data Found!<br></br>Click{" "}
+          <span className="text-PrimaryBlue font-bold underline">
+            <a href="/goals"> here</a>
+          </span>{" "}
+          to set<br></br>a new goal.
+        </h1>
+      );
+    } else {
+      return (
+        <div className="flex flex-col h-auto w-1/4 justify-center ml-32 pl-32">
+          <div className="flex h-3/4 justify-center">
+            <div className="w-32 h-32 flex justify-center content-center self-center absolute rounded-full bg-PrimaryGrey shadow-lg">
+              <span className="text-center self-center font-bold text-white">
+                {totalMilesRan}/<br></br>
+                {weeklyRunGoal}
+              </span>
+            </div>
+            <PieChart chartData={mileChartData} />
+          </div>
+          <span className="text-center py-4 text-white font-medium text-3xl">
+            Miles Ran
+          </span>
+        </div>
+      );
+    }
+  };
+
+  const renderCalories = () => {
+    if (weeklyCalorieGoal === 0) {
+      return (
+        <h1 className="text-PrimaryGrey space-y-2">
+          No Run Data Found!<br></br> Click{" "}
+          <span className="text-PrimaryBlue font-bold underline">
+            <a href="/goals"> here</a>
+          </span>{" "}
+          to set<br></br> a new goal.
+        </h1>
+      );
+    } else {
+      return (
+        <div className="flex flex-col h-auto w-1/4 mr-32 pr-32">
+          <div className="flex h-3/4 justify-center">
+            <div className="w-32 h-32 flex justify-center content-center self-center absolute rounded-full bg-PrimaryGrey shadow-lg">
+              <span className="text-center self-center font-bold text-white">
+                {totalCaloriesConsumed}/<br></br>
+                {weeklyCalorieGoal}
+              </span>
+            </div>
+            <PieChart chartData={calorieChartData} />
+          </div>
+          <span className="text-center py-4 text-white font-medium text-3xl">
+            Calorie Goal
+          </span>
+        </div>
+      );
+    }
+  };
+
   return (
     <>
       <Header />
@@ -149,34 +230,8 @@ const Home = () => {
       {/* /* -------------------------Chart JS-----------------------*/}
       <section className="flex justify-center flex-col h-full w-screen mt-16">
         <div className="flex relative h-96 w-full m-auto justify-center">
-          <div className="flex flex-col h-auto w-1/4 justify-center">
-            <div className="flex h-3/4 justify-center">
-              <div className="w-32 h-32 flex justify-center content-center self-center absolute rounded-full bg-PrimaryGrey shadow-lg">
-                <span className="text-center self-center font-bold text-white">
-                  {totalCaloriesConsumed}/<br></br>
-                  {weeklyCalorieGoal}
-                </span>
-              </div>
-              <PieChart chartData={calorieChartData} />
-            </div>
-            <span className="text-center py-4 text-3xl text-white font-medium">
-              Calorie Goal
-            </span>
-          </div>
-          <div className="flex flex-col h-auto w-1/4 justify-center">
-            <div className="flex h-3/4 justify-center">
-              <div className="w-32 h-32 flex justify-center content-center self-center absolute rounded-full bg-PrimaryGrey shadow-lg">
-                <span className="text-center self-center font-bold text-white">
-                  {totalMilesRan}/<br></br>
-                  {weeklyRunGoalState}
-                </span>
-              </div>
-              <PieChart chartData={mileChartData} />
-            </div>
-            <span className="text-center py-4 text-white font-medium text-3xl">
-              Miles Ran
-            </span>
-          </div>
+          {renderCalories()}
+          {renderMilesRan()}
         </div>
         <div className="flex justify-center w-3/4 h-80 m-auto">
           <LineChart chartData={WeightChartData} goalWeight={160} />
