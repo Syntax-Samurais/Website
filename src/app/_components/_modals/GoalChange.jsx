1c6582e35a62a2836532c5d644d9969bd32c1453
MWise707 <markezrawiseman@gmail.com>
2024-03-01 20:46:03 -0800

    close modal fix

--- src/app/_components/_modals/GoalChange.jsx
+++ src/app/_components/_modals/GoalChange.jsx
@@ -1,10 +1,14 @@
 "use client";
 
-import React, { useState, useEffect } from "react";
+import React from "react";
 
 export default function GoalChange({ showModal, handleCloseModal }) {
   const handleClose = () => {
     handleCloseModal();
+  };
+
+  const handleClick = (e) => {
+    e.stopPropagation();
   };
 
   return (
@@ -13,8 +17,12 @@ export default function GoalChange({ showModal, handleCloseModal }) {
         className={`fixed inset-0 flex justify-center items-center bg-black bg-opacity-50 ${
           showModal ? "" : "hidden"
         }`}
+        onClick={handleClose}
       >
-        <div className="bg-sky-900 p-8 rounded-md relative mx-4 my-4 sm:mx-8 sm:my-8 lg:mx-16 lg:my-16">
+        <div
+          className="bg-sky-900 p-8 rounded-md relative mx-auto my-auto w-3/4 h-3/4 sm:w-2/3 sm:h-2/3"
+          onClick={handleClick}
+        >
           <button
             onClick={handleClose}
             className="absolute top-2 right-2 text-white"
