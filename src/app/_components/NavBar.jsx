5df6553806544d89780b559975a603bf60fab0e6
derekmanninghm <derekmanninghm@gmail.com>
2024-03-04 09:43:57 -0800

    nav bar style changes

--- src/app/_components/NavBar.jsx
+++ src/app/_components/NavBar.jsx
@@ -9,40 +9,43 @@ const NavBar = () => {
   let selectedStyle =
     "flex justify-center h-full w-40 rounded-3xl bg-PrimaryGrey items-center font-bold text-white drop-shadow-lg hover:brightness-110";
   let defaultStyle =
-    "flex justify-center h-full w-40 rounded-3xl items-center hover:text-PrimaryGrey hover:font-bold";
+    "flex text-nowrap justify-center h-full w-40 rounded-3xl items-center hover:text-PrimaryGrey hover:font-bold";
 
   return (
     <>
-      <div className="flex mt-6 mx-auto w-3/4 h-10">
+      <div className="flex mt-6 h-10 w-11/12 sm:w-3/4 m-auto max-w-navWrap">
         <div className="w-14 h-14 inset-x-0 -my-2 m-auto absolute rounded-full bg-neutral-300 drop-shadow-lg z-10"></div>
         <div className="flex m-auto relative w-full h-full rounded-full bg-neutral-300">
-          <div className="flex justify-around content-center px-12 w-1/2 text-sm items-center">
+          <div className="flex justify-around content-center px-4 sm:px-12 w-1/2 text-sm items-center">
             <Link
               href="/Home"
               className={path == "/Home" ? selectedStyle : defaultStyle}
             >
-              Dashboard
+              {" "}
+              Dashboard{" "}
             </Link>
-            {/* update goals link href when route is created */}
             <Link
               href="/goals"
               className={path == "/goals" ? selectedStyle : defaultStyle}
             >
-              My Goals
+              {" "}
+              My Goals{" "}
             </Link>
           </div>
-          <div className="flex justify-around content-center px-12 w-1/2 text-sm items-center">
+          <div className="flex justify-around content-center px-4 sm:px-12 w-1/2 text-sm items-center">
             <Link
               href="/Diet"
               className={path == "/Diet" ? selectedStyle : defaultStyle}
             >
-              My Diet
+              {" "}
+              My Diet{" "}
             </Link>
             <Link
               href="/cardio"
               className={path == "/cardio" ? selectedStyle : defaultStyle}
             >
-              My Cardio
+              {" "}
+              My Cardio{" "}
             </Link>
           </div>
         </div>
