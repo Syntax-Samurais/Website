dd667d1b848a08aa98750d4250fe1829fd4459ee
MWise707 <markezrawiseman@gmail.com>
2024-03-07 09:52:16 -0800

    add dependency array to cardioRibbon and resize ribbons for narrow page

--- src/app/_components/CardioRibbon.jsx
+++ src/app/_components/CardioRibbon.jsx
@@ -29,7 +29,7 @@ export default function CardioRibbon({
   return (
     <div className="flex flex-wrap justify-center items-center pt-12">
       <div>
-        <div className="flex flex-col justify-center items-center text-white bg-PrimaryBlue border-[50px] border-transparent border-r-[#2e2f2e] border-l-[#2e2f2e] w-[70vw] h-[15vh] hover:bg-SecondaryBlue">
+        <div className="flex flex-col justify-center items-center text-white bg-PrimaryBlue border-[50px] border-transparent border-r-[#2e2f2e] border-l-[#2e2f2e] w-[100vw] md:w-[70vw] h-[15vh] hover:bg-SecondaryBlue">
           <div className="border-2 border-white p-2 mb-4 bg-Utility">
             <p className="flex flex-wrap text-center md:text-lg text-sm font-bold text-black">
               WEEKLY GOAL
--- src/app/_components/WeightRibbon.jsx
+++ src/app/_components/WeightRibbon.jsx
@@ -15,7 +15,14 @@ export default function WeightRibbon({
 
   useEffect(() => {
     updateWeightText();
-  }, [currentWeight, showModal, gain_weight, lose_weight, maintain_weight]);
+  }, [
+    currentWeight,
+    showModal,
+    gain_weight,
+    lose_weight,
+    maintain_weight,
+    goalWeight,
+  ]);
 
   function updateWeightText() {
     if (
@@ -42,7 +49,7 @@ export default function WeightRibbon({
   return (
     <div className="flex flex-wrap justify-center items-center pt-12">
       <div>
-        <div className="flex flex-col justify-center items-center text-white bg-PrimaryBlue border-[50px] border-transparent border-r-[#2e2f2e] border-l-[#2e2f2e] w-[70vw] h-[15vh] hover:bg-SecondaryBlue">
+        <div className="flex flex-col justify-center items-center text-white bg-PrimaryBlue border-[50px] border-transparent border-r-[#2e2f2e] border-l-[#2e2f2e] w-[100vw] md:w-[70vw] h-[15vh] hover:bg-SecondaryBlue">
           <div className="border-2 border-white p-2 mb-4 bg-Utility">
             <p className="flex flex-wrap text-center md:text-lg text-sm font-bold text-black">
               TARGET GOAL: {displayDate}
